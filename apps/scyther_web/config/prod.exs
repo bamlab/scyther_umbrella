8459a6c9f3538e49928e3b901c7a9da76e10d4ba
tychota <tychota@users.noreply.github.com>
2018-11-04 22:21:38 +0100

    :whale: (add docker-compose)

--- apps/scyther_web/config/prod.exs
+++ apps/scyther_web/config/prod.exs
@@ -60,7 +60,8 @@ config :scyther_web, ScytherWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+config :phoenix, :serve_endpoints, true
+
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
