0ff0dddaa1443b8b655534d2d14064292938206b
tychota <tychota@users.noreply.github.com>
2018-11-04 19:21:00 +0100

    :rocket: (distillery) add distillery

--- apps/scyther_web/config/prod.exs
+++ apps/scyther_web/config/prod.exs
@@ -14,9 +14,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :scyther_web, ScytherWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: {:system, "PORT"}],
+  url: [host: "localhost", port: {:system, "PORT"}],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:phoenix_distillery, :vsn)
 
 # ## SSL Support
 #
